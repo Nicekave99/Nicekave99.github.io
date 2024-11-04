44131183c3049f939240bdbecbdc12ab60dad4d1
NiceKave-star <nicekave47@gmaik.com>
2024-11-04 12:22:47 +0700

    Nakhonsawan Website Eiei

---
+++ main.js
@@ -0,0 +1,58 @@
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
+const menuBtnIcon = menuBtn.querySelector("i");
+
+menuBtn.addEventListener("click", (e) => {
+  navLinks.classList.toggle("open");
+
+  const isOpen = navLinks.classList.contains("open");
+  menuBtnIcon.setAttribute(
+    "class",
+    isOpen ? "ri-close-line" : "ri-menu-3-line"
+  );
+});
+
+navLinks.addEventListener("click", (e) => {
+  navLinks.classList.remove("open");
+  menuBtnIcon.setAttribute("class", "ri-menu-3-line");
+});
+
+const scrollRevealOption = {
+  distance: "50px",
+  origin: "bottom",
+  duration: 1000,
+};
+
+// container
+ScrollReveal().reveal(".container__content h1", {
+  ...scrollRevealOption,
+});
+ScrollReveal().reveal(".container__content h2", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+ScrollReveal().reveal(".container__content p", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+ScrollReveal().reveal(".socials", {
+  ...scrollRevealOption,
+  delay: 1500,
+});
+
+// จับทุกปุ่มใน .slider-nav
+const navButtons = document.querySelectorAll(".slider-nav a");
+const slider = document.querySelector(".slider");
+const slides = document.querySelectorAll(".slider img");
+
+// ฟังก์ชันเลื่อนไปยังสไลด์ที่ต้องการ
+navButtons.forEach((button, index) => {
+  button.addEventListener("click", (e) => {
+    e.preventDefault();
+    slides[index].scrollIntoView({
+      behavior: "smooth",
+      block: "nearest",
+      inline: "start",
+    });
+  });
+});
